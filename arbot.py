fe7fae0cffa4fb4ca7468dfe357b18e6132b5520
Ed Ye <ye.edward.zhijia@gmail.com>
2017-06-14 00:19:41 -0400

    POC for bittrex, poloniex

--- arbot.py
+++ arbot.py
@@ -1,4 +1,7 @@
 import requests
+import sched, time
+
+waitTime = 2
 
 def getOrders(coin, market):
         url = 'https://tuxexchange.com/api?method=getorders&coin=' + coin + '&market=' + market
@@ -7,9 +10,11 @@ def getOrders(coin, market):
 
 def displayJSONOrderSpread(data):
     ask = data['asks'][0][0]
+    ask_volume = data['asks'][0][1]
     bid = data['bids'][0][0]
-    print("lowest ask:", ask)
-    print("highest bid:", bid)
+    bid_volume = data['bids'][0][1]
+    print("lowest ask:", ask, ", volume:", ask_volume)
+    print("highest bid:", bid, ", volume:", bid_volume)
     print("spread: %.5f%%" % ((float(ask)/float(bid) - 1) * 100))
 
 def displayOrderSpread(coin, market):
@@ -18,7 +23,18 @@ def displayOrderSpread(coin, market):
     print('')
 
 
+s = sched.scheduler(time.time, time.sleep)
+def loop(sc):
 
-displayOrderSpread('DCR', 'BTC')
-displayOrderSpread('PEPECASH', 'BTC')
+    print("\n\nTime:", time.time())
+    print("Getting data...\n")
 
+    displayOrderSpread('DCR', 'BTC')
+    displayOrderSpread('PEPECASH', 'BTC')
+
+    s.enter(waitTime,1, loop, (sc,))
+
+s.enter(waitTime,1, loop, (s,))
+s.run()
+
+# bittrex poloniex ETH spread
